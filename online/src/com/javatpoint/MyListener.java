e2af92b79ea6adc1b33e4fe6e0d4224e20703cc4
Dhananjay Jain <dhanjay.jain@gmail.com>
2022-07-30 03:33:34 +0530

    New Development start

--- online/src/com/javatpoint/MyListener.java
+++ online/src/com/javatpoint/MyListener.java
@@ -70,5 +70,27 @@ public class MyListener implements ServletContextListener{
 		
 		System.out.println("Start deployement and start project");
 		
+		
+		// NEW DEVELOPMENT START
+		
+		String sql = "SELECT id, username FROM users WHERE id = ?";
+	    String url = "jdbc:sqlite:path-to-db-file/chinook/chinook.db";
+	    try (Connection conn = DriverManager.getConnection(url);
+	      Statement stmt = conn.createStatement()) {
+
+	      try {
+	        ResultSet rs = stmt.executeQuery("select * from albums");
+	        while (rs.next()) {
+	          String name = rs.getString("title");
+	          System.out.println(name);
+	        }
+	      } catch (SQLException e ) {
+	            throw new Error("Problem", e);
+	      } 
+
+	    } catch (SQLException e) {
+	      throw new Error("Problem", e);
+	    } 
+		
 	}
 }
